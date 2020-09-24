d591e62ca8ab4e8230ca3dc33e02880f3c260523
sventhevenn <kristiankramas@outlook.com>
2020-09-25 01:23:09 +0200

    Misc

--- src/App.jsx
+++ src/App.jsx
@@ -15,16 +15,8 @@ import {
 import { createPartiallyEmittedExpression } from "typescript";
 
 export default () => {
-  // const prescription = [
-  //   { name: "Ritalin MR", volume: 20, time: 8 },
-  //   { name: "Ritalin MR", volume: 10, time: 14 },
-  // ];
-
   const [pills, setPills] = useState(1);
-  const [prescription, setPrescription] = useState([
-    // { name: "Ritalin MR", volume: 20, time: 8 },
-    // { name: "Ritalin MR", volume: 10, time: 14 },
-  ]);
+  const [prescription, setPrescription] = useState();
 
   const { handleSubmit, register, errors } = useForm();
   const onSubmit = (values) => {
@@ -55,15 +47,17 @@ export default () => {
     setPrescription(newPrescription);
   };
 
-  console.log(prescription);
-
   return (
     <>
       <Container>
         <Card className="mt-3">
           <Card.Body>
             <Graph prescription={prescription} />
-            <Form onChange={handleSubmit(onSubmit)}>
+            <Form
+              onChange={handleSubmit(onSubmit)}
+              onBlur={handleSubmit(onSubmit)}
+              onSubmit={handleSubmit(onSubmit)}
+            >
               {[...Array(pills).keys()].map((index) => {
                 return (
                   <>
@@ -75,16 +69,23 @@ export default () => {
                         name={`name-${index}`}
                         ref={register({ required: true })}
                       >
+                        {!prescription && <option value={null}></option>}
                         <option>Ritalin MR</option>
                         <option>Ritalin IR</option>
                       </Form.Control>
                     </Form.Group>
                     <Form.Group controlId={`volume-${index}`}>
-                      <Form.Label>Volume {index + 1}</Form.Label>
                       <InputGroup>
+                        {/* <InputGroup.Prepend>
+                          <InputGroup.Text>
+                            Volume {index + 1}
+                          </InputGroup.Text>
+                        </InputGroup.Prepend> */}
                         <Form.Control
                           type="number"
-                          step="1"
+                          step="10"
+                          min={0}
+                          max={1000}
                           defaultValue={20}
                           name={`volume-${index}`}
                           ref={register({ required: true })}
@@ -95,37 +96,54 @@ export default () => {
                       </InputGroup>
                     </Form.Group>
                     <Form.Group controlId={`time-${index}`}>
-                      <Form.Label>Time {index + 1}</Form.Label>
-                      <Form.Control
-                        as="select"
-                        name={`time-${index}`}
-                        ref={register({ required: true })}
-                      >
-                        {[...Array(24).keys()].map((hour) => (
-                          <option>{hour}</option>
-                        ))}
-                      </Form.Control>
+                      <InputGroup>
+                        <InputGroup.Prepend>
+                          <InputGroup.Text>
+                            Time
+                          </InputGroup.Text>
+                        </InputGroup.Prepend>
+                        <Form.Control
+                          as="select"
+                          name={`time-${index}`}
+                          ref={register({ required: true })}
+                        >
+                          {[...Array(24).keys()].map((hour) => (
+                            <option>{hour}</option>
+                          ))}
+                        </Form.Control>
+                      </InputGroup>
                     </Form.Group>
                   </>
                 );
               })}
+              <Row cla>
+                <Col>
+                  <Button
+                    type="submit"
+                    className="w-100"
+                    onClick={() => {
+                      setPills(pills + 1);
+                      handleSubmit(onSubmit);
+                    }}
+                  >
+                    Add medicine
+                  </Button>
+                </Col>
+                <Col>
+                  <Button
+                    type="submit"
+                    className="w-100"
+                    onClick={() => {
+                      setPills(pills - 1);
+                      handleSubmit(onSubmit);
+                    }}
+                    disabled={pills === 1}
+                  >
+                    Remove medicine
+                  </Button>
+                </Col>
+              </Row>
             </Form>
-            <Row cla>
-              <Col>
-                <Button className="w-100" onClick={() => setPills(pills + 1)}>
-                  Add medicine
-                </Button>
-              </Col>
-              <Col>
-                <Button
-                  className="w-100"
-                  onClick={() => setPills(pills - 1)}
-                  disabled={pills === 1}
-                >
-                  Remove medicine
-                </Button>
-              </Col>
-            </Row>
           </Card.Body>
         </Card>
       </Container>
